9cb81997df9d6747025e1089604fc1f92ae2a98a
Chinyere <chinyereunamba17@gmail.com>
2025-01-15 02:28:09 +0100

    blog section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Intro, Join, Services, WhyPay4Me } from "@/components/layout";
+import Posts from "@/components/layout/blog-post";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <Intro />
       <WhyPay4Me />
       <Services />
+      <Posts />
       <Join />
     </main>
   );
